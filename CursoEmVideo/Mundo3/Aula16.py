a2b7fdbaf160b5568874445c2d721bd96e77d4c6
Vinícius Pizzi <pizzivinicius@gmail.com>
2025-12-08 15:00:43 -0300

    Aula16

---
+++ CursoEmVideo/Mundo3/Aula16.py
@@ -0,0 +1,26 @@
+lanche = ("Hamburguer", "Suco", "Pizza", "Pudim")
+
+print(lanche[0]) #Pega o primeiro elemento
+print(lanche[-2]) #Pega o penultimo elemento
+print(lanche[1:3]) #Desconsidera o ultimo elemento
+print(lanche[2:]) #Elemento 2 ate o final
+print(lanche[:2]) #Pega do primeiro elemento ate o elemento 1
+print(lanche[-3:]) #Começa no suco e vai até o final
+print(len(lanche)) #Mostra o numero de itens
+print(sorted(lanche)) #Mostra a lista organizada
+
+print("-"*20)
+
+for c in lanche:
+    print(c)
+    #exibe cada item na tupla
+
+print("-"*20)
+
+for cont in range(0, len(lanche)):
+    print(lanche[cont])
+
+print("-"*20)
+
+for pos, comida in enumerate(lanche):
+    print(f"posição {pos}, comida {comida}")
